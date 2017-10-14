c56689240276a7e9efe397157aab1a104b88c856
Ester Lorente <ester.lorente@est.fib.upc.edu>
2017-10-14 18:27:13 +0200

    Photo Answer activity init

--- android/app/src/main/java/dev/blind/hackupc/a2017/blindhelper/MainActivityFragmentHelper.java
+++ android/app/src/main/java/dev/blind/hackupc/a2017/blindhelper/MainActivityFragmentHelper.java
@@ -60,9 +60,8 @@ public class MainActivityFragmentHelper extends Fragment {
     private void setUpListeners() {
         buttonPhoto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //makePhotoCamera();
-                //Intent intent = new Intent(getActivity(), WhereIAmActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(getActivity(), PhotoAnswerActivity.class);
+                startActivity(intent);
             }
         });
 
